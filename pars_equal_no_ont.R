8a3fddddb06864cca00fd22a641a4f1498c0fa44
P.M. Santos neves <p282067@WORKSPACE.RUG.NL>
2018-07-27 16:42:23 +0200

    Removes NA and plots

--- pars_equal_no_ont.R
+++ pars_equal_no_ont.R
@@ -1,7 +1,8 @@
-files <- list.files()
 library(ggplot2)
-grep("no_ontogeny",files)
-grep()
+
+setwd("G:/My Drive/PhD/Projects/DAISIE/DAISIE_results/results/pars_equal/FORTRAN")
+
+files <- list.files()
 
 no_ont_filnames <- files[grep("no_ontogeny",files)]
 
@@ -9,11 +10,11 @@ for (i in seq_along(no_ont_filnames)) {
   load(no_ont_filnames[i])
   assign(tools::file_path_sans_ext(no_ont_filnames[i]), no_ontogeny_CS_pars_equal_ML)
 }
+no_ontogeny_CS_pars_equal_ML_11 <- no_ontogeny_CS_pars_equal_ML_11[1]
+setwd("G:/My Drive/PhD/Projects/DAISIE/DAISIE_results/DAISIE_analysis")
 
-ls()
-View(data.frame(unlist(no_ontogeny_CS_pars_equal_ML_1)))
 
-do.call(rbind.data.frame, no_ontogeny_CS_pars_equal_ML_1)
+
 no_ontogeny_CS_df <- data.frame()
 for (i in 1:11) {
   no_ontogeny_CS_df <- rbind(no_ontogeny_CS_df, do.call(rbind.data.frame, get(paste0("no_ontogeny_CS_pars_equal_ML_", i))))
@@ -30,12 +31,13 @@ tail(final_no_ont$mu)
 mu_plot_no_ontogeny <- ggplot(data = final_no_ont, aes(x = mu)) + 
   geom_histogram(fill = "deepskyblue") +
   geom_vline(xintercept = 2.2, color = "orange2", size = 1.2) +
-  ggtitle("Estimates of Î¼ \nwithout island ontogeny") +
-  xlab("Î¼") + ylab("Frequency") +
-  theme(axis.text=element_text(size=14),
-        axis.title=element_text(size=14),
-        plot.title=element_text(size=16,face="bold"),
+  ggtitle("Estimates of µ \nwithout island ontogeny") +
+  xlab("µ") + ylab("Frequency") +
+  theme(axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        plot.title = element_text(size = 16,face = "bold"),
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")
   )
 
 median(final_no_ont$mu, na.rm = T)
+sd(final_no_ont$mu, na.rm = T)
