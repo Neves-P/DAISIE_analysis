82117cf4ce96fc1e0a2a68efcddecc22a6e5102f
Neves-P <p.m.santos.neves@rug.nl>
2019-07-16 16:01:28 +0200

    add long time analysis

--- basic_scenarios_MLE_sims.R
+++ basic_scenarios_MLE_sims.R
@@ -3,6 +3,7 @@ download_results()
 res_low_clado <- load_DAISIE_results(sim_file_name = "sim-DAISIE-1-6-1000-7.48223e-05-1-0.000740740740740741-0.001-1-1000-0.1-0.11-13500-0.1-1-9.RData", ontogeny = TRUE)
 res_med_clado <- load_DAISIE_results(sim_file_name = "sim-DAISIE-1-6-1000-7.48223e-05-1-0.00222222222222222-0.001-1-1000-0.1-0.11-13500-0.1-1-9.RData", ontogeny = TRUE)
 res_high_clado <- load_DAISIE_results(sim_file_name = "sim-DAISIE-1-6-1000-7.48223e-05-1-0.00740740740740741-0.001-1-1000-0.1-0.11-13500-0.1-1-9.RData", ontogeny = TRUE)
+res_long_time <- load_DAISIE_results(sim_file_name = "sim-DAISIE-1-7-1000-7.48223e-05-1-0.05-0.001-1-1000-0.1-15-13500-0.1-1-9.RData", ontogeny = TRUE)
 res_low_clado[which(res_low_clado$lambda_c < 10)]
 
 
@@ -185,3 +186,19 @@ for (i in 1:10) {
   )
 }
 
+# long time
+for (i in 1:1000) {
+  execute_next_setup(
+    time = 7,
+    M = 1000,
+    lac = res_long_time$lambda_c[which(res_long_time$lambda_c < 10)][i],
+    mu = res_long_time$mu[which(res_long_time$lambda_c < 10)][i],
+    K = res_long_time$K[which(res_long_time$lambda_c < 10)][i],
+    gam = res_long_time$gamma[which(res_long_time$lambda_c < 10)][i],
+    laa = res_long_time$lambda_a[which(res_long_time$lambda_c < 10)][i],
+    island_ontogeny = FALSE ,
+    branch = "@develop",
+    replicates = 10,
+    complete_analysis = FALSE
+  )
+}
