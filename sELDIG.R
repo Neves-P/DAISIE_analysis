aa09d62cedf145b8224dbd1c38148b73811c762b
Neves-P <p.m.santos.neves@rug.nl>
2019-07-26 20:46:20 +0200

    run high k sims

--- sELDIG.R
+++ sELDIG.R
@@ -1,46 +1,46 @@
 # sELDIG simulations
 
 
-# CS
-# execute_next_setup(
-#   project_name = get_project_name(),
-#   max_sims = ,
-#   account = get_p_number(),
-#   download_files = FALSE,
-#   partition = "regular",
-#   time = 10,
-#   M = 1000,
-#   lac = c(0.9, 1, 1.3),
-#   mu = c(0.3, 0.4, 0.6),
-#   K = c(5, 15, Inf),
-#   gam = c(0.0001, 0.0005, 0.001),
-#   laa = c(0.2, 0.4, 0.6),
-#   island_ontogeny = "const",
-#   replicates = 1000,
-#   complete_analysis = FALSE,
-#   branch = "@develop"
-# )
-#
-# # # IW
-# execute_next_setup(
-#   project_name = get_project_name(),
-#   max_sims = ,
-#   account = get_p_number(),
-#   download_files = FALSE,
-#   partition = "regular",
-#   time = 10,
-#   M = 1000,
-#   lac = c(0.9, 1, 1.3),
-#   mu = c(0.3, 0.4, 0.6),
-#   K = c(5, 15, Inf),
-#   gam = c(0.0001, 0.0005, 0.001),
-#   laa = c(0.2, 0.4, 0.6),
-#   island_ontogeny = "const",
-#   replicates = 100,
-#   complete_analysis = FALSE,
-#   branch = "@master",
-#   divdepmodel = 2
-# )
+CS
+execute_next_setup(
+  project_name = get_project_name(),
+  max_sims = ,
+  account = get_p_number(),
+  download_files = FALSE,
+  partition = "regular",
+  time = 10,
+  M = 1000,
+  lac = c(0.9, 1, 1.3),
+  mu = c(0.3, 0.4, 0.6),
+  K = c(5, 15, Inf),
+  gam = c(0.0001, 0.0005, 0.001),
+  laa = c(0.2, 0.4, 0.6),
+  island_ontogeny = "const",
+  replicates = 1000,
+  complete_analysis = FALSE,
+  branch = "@develop"
+)
+
+# # IW
+execute_next_setup(
+  project_name = get_project_name(),
+  max_sims = ,
+  account = get_p_number(),
+  download_files = FALSE,
+  partition = "regular",
+  time = 10,
+  M = 1000,
+  lac = c(0.9, 1, 1.3),
+  mu = c(0.3, 0.4, 0.6),
+  K = c(5, 15, Inf),
+  gam = c(0.0001, 0.0005, 0.001),
+  laa = c(0.2, 0.4, 0.6),
+  island_ontogeny = "const",
+  replicates = 100,
+  complete_analysis = FALSE,
+  branch = "@master",
+  divdepmodel = 2
+)
 #
 # test_ntips <- DAISIE_sim(10, 1000, c(0.9, 0.4, Inf, 0.0005, 0.4), 100)
 
@@ -159,3 +159,49 @@ get_newick_trees <- function(scenario = "CS",
   }
 
 }
+
+# Higher K
+
+
+# CS
+execute_next_setup(
+  project_name = get_project_name(),
+  max_sims = ,
+  account = get_p_number(),
+  download_files = FALSE,
+  partition = "regular",
+  time = 10,
+  M = 1000,
+  lac = c(0.9, 1, 1.3),
+  mu = c(0.3, 0.4, 0.6),
+  K = c(30),
+  gam = c(0.0001, 0.0005, 0.001),
+  laa = c(0.2, 0.4, 0.6),
+  island_ontogeny = "const",
+  replicates = 1000,
+  complete_analysis = FALSE,
+  branch = "@develop", force = TRUE
+)
+
+# IW
+execute_next_setup(
+  project_name = get_project_name(),
+  max_sims = ,
+  account = get_p_number(),
+  download_files = FALSE,
+  partition = "regular",
+  time = 10,
+  M = 1000,
+  lac = c(0.9, 1, 1.3),
+  mu = c(0.3, 0.4, 0.6),
+  K = c(30),
+  gam = c(0.0001, 0.0005, 0.001),
+  laa = c(0.2, 0.4, 0.6),
+  island_ontogeny = "const",
+  replicates = 100,
+  complete_analysis = FALSE,
+  branch = "@master",
+  divdepmodel = 2, force = TRUE
+)
+
+
